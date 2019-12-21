9170bacd59b1cd8326a38500345ca9598fd81582
Unknown <kmkhaha@nate.com>
2019-12-21 21:09:33 +0900

    [1.StackView] add UIStackView example

--- MKStudy/View/StackViewViewController.swift
+++ MKStudy/View/StackViewViewController.swift
@@ -10,13 +10,84 @@ import UIKit
 
 class StackViewViewController: UIViewController {
 
+    // MARK: - InterfaceBuilder Outlet
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    
+    @IBAction func actionAddView(_ sender: Any) {
+        let stack = stackView
+        let index = stackView.arrangedSubviews.count - 1
+        let addView = stack?.arrangedSubviews[index]
+        let scroll = scrollView
+        let offset = CGPoint(x: scroll?.contentOffset.x ?? 0, y: (scroll?.contentOffset.y ?? 0) + (addView?.frame.size.height ?? 0))
+        let newView = createEntry()
+        newView.isHidden = true
+        stack?.insertArrangedSubview(newView, at: index)
+        UIView.animate(withDuration: 0.25) {
+            newView.isHidden = false
+            scroll?.contentOffset = offset
+        }
+    }
+    
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let insets = UIEdgeInsets(top: 20.0, left: 0.0, bottom: 0.0, right: 0.0)
+        scrollView.contentInset = insets
+        scrollView.scrollIndicatorInsets = insets
+    }
+}
+
+private extension StackViewViewController {
+    
+    func createEntry() -> UIView {
+        let date = DateFormatter.localizedString(from: Date(), dateStyle: .short, timeStyle: .none)
+        let number = "\(randomHexQuad())-\(randomHexQuad())-\(randomHexQuad())-\(randomHexQuad())"
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.alignment = .firstBaseline
+        stack.distribution = .fill
+        stack.spacing = 8
+        let dateLabel = UILabel()
+        dateLabel.text = date
+        dateLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        let numberLabel = UILabel()
+        numberLabel.text = number
+        numberLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        let deleteButton = UIButton(type: .roundedRect)
+        deleteButton.setTitle("Delete", for: .normal)
+        deleteButton.addTarget(self, action: #selector(self.deleteStackView(sender:)), for: .touchUpInside)
+        stack.addArrangedSubview(dateLabel)
+        stack.addArrangedSubview(numberLabel)
+        stack.addArrangedSubview(deleteButton)
+        
+        return stack
     }
     
-
+    func randomHexQuad() -> String {
+        return NSString(format: "%X%X%X%X",
+                        arc4random() % 16,
+                        arc4random() % 16,
+                        arc4random() % 16,
+                        arc4random() % 16
+            ) as String
+    }
+    
+    @objc func deleteStackView(sender: UIButton) {
+        if let view = sender.superview {
+            UIView.animate(withDuration: 0.25, animations: {
+                view.isHidden = true
+            }) { (success) in
+                view.removeFromSuperview()
+            }
+        }
+    }
+    
 }
 
 extension StackViewViewController: initWithStoryboardDelegate {
